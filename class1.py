c6ada3ed95631ee41fc35a1c2d51bde6e33c5219
Ratan Prakash Sah <sahratan654@gmail.com>
2026-03-29 12:09:28 +0545

    upload python files

---
+++ class1.py
@@ -0,0 +1,24 @@
+principle=int(input("enter principle"))
+time=int(input("enter a time for year:"))
+rate=float(input("enter rate"))
+si=(principle*time*rate)/100
+print(si)
+
+
+
+
+
+
+
+# # greatest three number
+# principle=int(input("enter first number"))
+# time=int(input("enter second number"))
+# rate=int(input("enter third number"))
+# if(principle>time):
+#     if(principle>rate):
+#         print("principle is greater")
+        
+#     elif(time>rate):
+#         print("time is greater")
+#     else:
+#         print("rate is greater")
